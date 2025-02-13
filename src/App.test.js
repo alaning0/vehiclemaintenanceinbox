90bebe16c7583d494575749b8bd531894d73a9e9
Alan Ing <thepoisonapple2004@gmail.com>
2025-02-13 19:48:02 +1300

    fix tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,10 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import '@testing-library/jest-dom';
+import Header from "./components/Header";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('displays "Vehicle Maintenance Inbox"', () => {
+  render(<Header />);
+  const headerElement = screen.getByText(/Vehicle Maintenance Inbox/i);
+  expect(headerElement).toBeInTheDocument();
 });
